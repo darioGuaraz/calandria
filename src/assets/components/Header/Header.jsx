3e8efb261f1af986a212f8697b8d8b14eae573d7
eldaTacenter <darioguaraz@gmail.com>
2025-09-22 11:36:47 -0300

    encaro de diseño definitivo

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import "./Header.css";
 import logoHeader from "../../img/logo-letras-blanco.png";
-import logoWspFlotante from "../../img/wspico.png";
+import tarjetaCalandria from "../../img/tarjeta-calandria.png";
+
+import headerFondo from "../../img/header-fondo.mp4";
+
 const Header = () => {
   const headerRef = useRef(null);
 
@@ -9,15 +12,14 @@ const Header = () => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
+          const logo = entry.target.querySelector(".header-logo");
+          const title = entry.target.querySelector(".title");
           if (entry.isIntersecting) {
-            const title = entry.target.querySelector("img");
-            const description = entry.target.querySelector("h1");
-
-            setTimeout(() => title.classList.add("visible"), 300);
-            setTimeout(() => description.classList.add("visible"), 800);
+            setTimeout(() => logo && logo.classList.add("visible"), 300);
+            setTimeout(() => title && title.classList.add("visible"), 800);
           } else {
-            const elements = entry.target.querySelectorAll("img, h1");
-            elements.forEach((el) => el.classList.remove("visible"));
+            if (logo) logo.classList.remove("visible");
+            if (title) title.classList.remove("visible");
           }
         });
       },
@@ -33,13 +35,26 @@ const Header = () => {
 
   return (
     <section ref={headerRef} className="header" id="header">
-      <div className="container-logo">
-        <img src={logoHeader} alt="Calandria Logo" className="header-logo" />
-      </div>
-      <div className="container-text-button">
-        <h1>
-          Soluciones digitales <br /> & estrategias de <br /> marketing
-        </h1>
+      <video
+        src={headerFondo}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="video-header"
+      ></video>
+      <div className="container-general">
+        <div className="tarjeta-img">
+          <img src={tarjetaCalandria} alt="" />
+        </div>
+        <div className="container-logo">
+          <img src={logoHeader} alt="Calandria Logo" className="header-logo" />
+          <div className="container-text">
+            <h1 className="title">
+              Soluciones digitales <br /> & estrategias de <br /> marketing
+            </h1>
+          </div>
+        </div>
       </div>
     </section>
   );
